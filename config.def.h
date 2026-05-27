8f4c552395e04a2c7fc70f42037edcf0ffe9f207
adelbouras <eyyad.kareem1212@gmail.com>
2026-05-27 02:44:34 +0100

    dwm configured

--- config.def.h
+++ config.def.h
@@ -40,7 +40,7 @@ static const int resizehints =
 static const int lockfullscreen =
     1; /* 1 will force focus on the fullscreen window */
 static const int refreshrate =
-    120; /* refresh rate (per second) for client move/resize */
+    60; /* refresh rate (per second) for client move/resize */
 
 static const Layout layouts[] = {
     /* symbol     arrange function */
